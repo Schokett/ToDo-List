c2d3a858fa9d6578e43a8d7cc7488ca8fad11934
schokett <skrollanelenazimmermann@gmail.com>
2026-06-15 10:03:17 +0200

    Start folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import Todo from "./views/todo/Todo";
 
 function App() {
-  return (
-    <div className="todo">
-      <div className="todo__action-container line">
-        <input type="text" className="todo__input" />
-        <button className="todo__add-btn">Add</button>
-      </div>
-      <div className="todo__todo-list-conatiner line">
-        <div className="todo__card-container">
-          <div className="todo__card">
-            <input type="checkbox" className="todo__input-checkbox"></input>
-            <p className="todo__title">Test</p>
-            <button className="todo__delete-btn">Löschen</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return;
+  <Todo />;
 }
 
 export default App;
